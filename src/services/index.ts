fbe43dbd1bbfc7d6ab35d6ddca3a1f83dec0f68c
Matthew_Pfister <matthew.pfister@slalom.com>
2019-12-13 15:06:49 -0600

    adding debugging statements

--- src/services/index.ts
+++ src/services/index.ts
@@ -37,12 +37,18 @@ const buildResponse = (season: AttributeMap): SeasonResponse => {
 const buildDetailResponse = async (season: AttributeMap): Promise<SeasonDetailsResponse> => {
   const seasonNode = seasonMapper.toNode(season);
 
+  console.log(JSON.stringify(season));
+
   const playerIds = seasonNode.playerIds || [];
   const playerPromises = playerIds.map((playerId) => playerClient.fetchByKey({ playerId }));
   const playerResults = await Promise.all(playerPromises);
   const playerNodes = playerResults.map(playerMapper.toNode);
 
+  console.log(JSON.stringify(playerNodes));
+
   const setResult = await requestClient.get(`https://api.scryfall.com/sets/${seasonNode.setCode}`);
+
+  console.log(JSON.stringify(setResult));
 
   return {
     ...seasonMapper.toView(seasonNode),
